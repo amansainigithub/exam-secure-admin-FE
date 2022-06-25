8ba929e64b2da668224c6f363dae74549a67b431
aman <amansainigithub>
2022-06-26 00:39:10 +0530

    change url

--- src/app/_helpers/authUrlHelper/base-url.service.ts
+++ src/app/_helpers/authUrlHelper/base-url.service.ts
@@ -5,7 +5,8 @@ import { Injectable } from '@angular/core';
 })
 export class BaseUrlService {
 
-  public baseUrl:string = 'http://localhost:5000/api/auth/cred/oex/secure/';
+  // public baseUrl:string = 'http://localhost:5000/api/auth/cred/oex/secure/';
+  public baseUrl:string = 'https://146.190.233.4:5000/api/auth/cred/oex/secure/';
   //  public baseUrl:string = 'http://examsecure-env.eba-3xmk9bvg.us-east-1.elasticbeanstalk.com/api/auth/cred/oex/secure/';
 
 
