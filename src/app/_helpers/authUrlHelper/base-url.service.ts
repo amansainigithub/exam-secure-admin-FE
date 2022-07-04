5533d341602cb4422a8f067de4edbbf6130604d6
aman <amansainigithub>
2022-07-05 01:55:54 +0530

    URLS

--- src/app/_helpers/authUrlHelper/base-url.service.ts
+++ src/app/_helpers/authUrlHelper/base-url.service.ts
@@ -9,7 +9,6 @@ export class BaseUrlService {
   // public baseUrl:string = 'http://103.93.16.121:33146/grabquiz/api/auth/cred/oex/secure/';
   public baseUrl:string = 'https://grabquiz.tech/api/auth/cred/oex/secure/';
 
- 
   constructor() { }
 }
  
